9c53c22330352d1788cf481eeba0180f1f481d0b
nFerragut88 <nathanferragut@gmail.com>
2019-09-28 19:48:15 -0400

    testing firestore

--- JavaScript.js
+++ JavaScript.js
@@ -1,29 +1,8 @@
 var busy = true;
 var hoverNull = false;
 
-const Firestore = require('@google-cloud/firestore');
 
-const db = new Firestore({
-  projectId: 'feisty-ward-254300',
-  keyFilename: 'feisty-ward-254300-e730983e4e17.json',
-});
 
-let docRef = db.collection('users').doc('alovelace');
-
-let setAda = docRef.set({
-  first: 'Ada',
-  last: 'Lovelace',
-  born: 1815
-});
-
-let aTuringRef = db.collection('users').doc('aturing');
-
-let setAlan = aTuringRef.set({
-  'first': 'Alan',
-  'middle': 'Mathison',
-  'last': 'Turing',
-  'born': 1912
-});
 
 $(document).ready(function(){
 	start();
